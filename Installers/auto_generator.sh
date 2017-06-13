6021892b7676cfb4842a4f9d8d2998ab551a83fb
root <root@nagios-server.c.project-y-164217.internal>
2017-06-13 11:20:22 +0000

    updated

--- Installers/auto_generator.sh
+++ Installers/auto_generator.sh
@@ -14,16 +14,37 @@ do
 	then
 		#if the IP is already in the Nagios
 		echo "Ip already Included"
+		
+		#send a message to cellphone number
+		echo "No changes in the Network - all servers including in the monitoring" | mail -s "project-Y" 18328710948@tmomail.net
 	else
 		#if the Ip is not create the configuration file for Nagios
 		echo "Creating configuration"
 
 		#obtain the name of the host
 		host=$(gcloud compute instances list | grep $Ip | cut -d' ' -f1)
+		
 		#execute the genrator file
 		bash /home/yojetoga/Nagios/Installers/generator.sh $host $Ip
 		
+		#copy the nagios_host file in the instance
+		gcloud compute copy-files /home/yojetoga/Nagios/Installers/Nagios_hosts yojetoga@$host:/home/yojetoga/
+		gcloud compute copy-files /home/yojetoga/plugins.repo yojetoga@$host:/home/yojetoga/		
+
+		#execute the installation of the repo and nagios
+		gcloud compute ssh yojetoga@$host --command "sudo cp /home/yojetoga/plugins.repo /etc/yum.repos.d/plugins.repo"
+		gcloud compute ssh yojetoga@$host --command "sudo yum repolist"
+		gcloud compute ssh yojetoga@$host --command "sudo bash Nagios_hosts"
+		gcloud compute ssh yojetoga@$host --command "sudo yum -y install plugins"
+		gcloud compute ssh yojetoga@$host --command "sudo systemctl restart nrpe"
+		
+		systemctl restart nrpe nagios httpd
+		
+		#send a message to a cellphone number to confirm the changes
+		echo "Changes in the Network - hots: $host - $Ip were added to the network and configuration has been installed - Instance included in the monitoring" | mail -s "project-y" 18328710948@tmomail.net
+		
 		#save the IP in the Nagios-server
 		echo $Ip >> /Network/Ips.txt
+
 	fi
 done
