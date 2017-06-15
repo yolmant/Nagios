74e449963df398a781c367fca707780595f378e1
root <root@nagios-server.c.project-y-164217.internal>
2017-06-15 19:44:01 +0000

    updated

--- Installers/auto_generator.sh
+++ Installers/auto_generator.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #check the list of IPs of the instances in the network 
-list=$(gcloud compute instances list | tail -n+2 | awk '{print $4}')
+list=$(sudo gcloud compute instances list | tail -n+2 | awk '{print $4}')
 
 #a loop that take each IP to be verified
 for ((i=1; i<=$(echo "$list" | wc -w); i++))
@@ -10,39 +10,38 @@ do
 	Ip=$(echo "$list" | sed -n "$i p")
 
 	#verify the IP from the file IPs.txt created in Network directory using mkdir /Network
-	if  grep -R $Ip /Network/Ips.txt
+	if grep $Ip /Network/Ips.txt
 	then
 		#if the IP is already in the Nagios
 		echo "Ip already Included"
-		
 	else
 		#if the Ip is not create the configuration file for Nagios
 		echo "Creating configuration"
 
 		#obtain the name of the host
-		host=$(gcloud compute instances list | grep $Ip | cut -d' ' -f1)
+		host=$(sudo gcloud compute instances list | grep $Ip | cut -d' ' -f1)
 		
 		#execute the genrator file
-		bash /home/yojetoga/Nagios/Installers/generator.sh $host $Ip
+		sudo bash /home/yojetoga/Nagios/Installers/generator.sh $host $Ip
 		
 		#copy the nagios_host file in the instance
-		gcloud compute copy-files /home/yojetoga/Nagios/Installers/Nagios_hosts yojetoga@$host:/home/yojetoga/
-		gcloud compute copy-files /home/yojetoga/plugins.repo yojetoga@$host:/home/yojetoga/		
+		sudo gcloud compute copy-files /home/yojetoga/Nagios/Installers/Nagios_hosts yojetoga@$host:/home/yojetoga/
+		sudo gcloud compute copy-files /home/yojetoga/plugins.repo yojetoga@$host:/home/yojetoga/		
 
 		#execute the installation of the repo and nagios
-		gcloud compute ssh yojetoga@$host --command "sudo cp /home/yojetoga/plugins.repo /etc/yum.repos.d/plugins.repo"
-		gcloud compute ssh yojetoga@$host --command "sudo yum repolist"
-	 	gcloud compute ssh yojetoga@$host --command "sudo bash Nagios_hosts"
-		gcloud compute ssh yojetoga@$host --command "sudo yum -y install plugins"
-		gcloud compute ssh yojetoga@$host --command "sudo systemctl restart nrpe"
+		sudo gcloud compute ssh yojetoga@$host --command "sudo cp /home/yojetoga/plugins.repo /etc/yum.repos.d/plugins.repo"
+		sudo gcloud compute ssh yojetoga@$host --command "sudo yum repolist"
+	 	sudo gcloud compute ssh yojetoga@$host --command "sudo bash Nagios_hosts"
+		sudo gcloud compute ssh yojetoga@$host --command "sudo yum -y install plugins"
+		sudo gcloud compute ssh yojetoga@$host --command "sudo systemctl restart nrpe"
 		
-		systemctl restart nrpe nagios httpd
+		sudo systemctl restart nrpe nagios httpd
 		
 		#send a message to a cellphone number to confirm the changes
-		echo "Changes in the Network - $host - $Ip were added to the network and configurated" | mail -s "project-y" *********@tmomail.net
+		echo "Changes in the Network - $host - $Ip were added to the network and configurated" | mail -s "project-y" 18328710948@tmomail.net
 		
 		#save the IP in the Nagios-server
-		echo $Ip >> /Network/Ips.txt
+		sudo sh -c "echo '$Ip' >> /Network/Ips.txt"
 
 	fi
 done
