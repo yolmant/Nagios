bf272f05393dd4703a5eeb49ecdc6586e5918590
root <root@nagios-server.c.project-y-164217.internal>
2017-06-15 19:43:30 +0000

    updated

--- Installers/remover.sh
+++ Installers/remover.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 #remove the IP from the file
-sed -i "s/$2//g" /Network/Ips.txt
+sed -i "s/$2//g" /Network/Ips.tx
+
+#remove blank spaces in the file
+sed -i 's/[\t ]//g;/^$/d' /Network/Ips.txt 
 
 #remove the nagios configuration of the host
 rm -rf /etc/nagios/conf.d/$1.cfg
